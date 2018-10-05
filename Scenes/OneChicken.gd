1d52d89c1023cf4f4b01f3d500dac4fc975052d9
Jaybomb <jaybopper@hotmail.com>
2018-10-06 00:50:08 +0100

    Added speedup

--- Scenes/OneChicken.gd
+++ Scenes/OneChicken.gd
@@ -3,7 +3,8 @@ extends Sprite
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
-export(int) var speed
+export(int) var defaultSpeed
+var shiftSpeedMultiplier = 10
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
@@ -11,13 +12,17 @@ func _ready():
 	pass
 
 func _process(delta):
+	var currentSpeed = defaultSpeed
+	if Input.is_key_pressed(KEY_SHIFT):
+		currentSpeed = currentSpeed * shiftSpeedMultiplier
+	
 	if Input.is_action_pressed("ui_left"):
-		position.x -= speed
+		position.x -= currentSpeed
 	if Input.is_action_pressed("ui_right"):
-		position.x += speed
+		position.x += currentSpeed
 	if Input.is_action_pressed("ui_up"):
-		position.y -= speed
+		position.y -= currentSpeed
 	if Input.is_action_pressed("ui_down"):
-		position.y += speed
+		position.y += currentSpeed
 	pass
 	
