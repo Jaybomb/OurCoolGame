a230a148dc5713b58f007315edf1595e00bb4ff4
Robin Grindrod <robingrindrod@live.ie>
2018-10-05 23:31:42 +0100

    Add movement

--- Scenes/OneChicken.gd
+++ Scenes/OneChicken.gd
@@ -11,5 +11,13 @@ func _ready():
 
 func _process(delta):
 	#if BUTTON_LEFT.isPressed()
+	if Input.is_action_pressed("ui_left"):
+		position.x -= 1
+	if Input.is_action_pressed("ui_right"):
+		position.x += 1
+	if Input.is_action_pressed("ui_up"):
+		position.y -= 1
+	if Input.is_action_pressed("ui_down"):
+		position.y += 1
 		
 	pass
