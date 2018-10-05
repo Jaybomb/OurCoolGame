5b79745043d9e25405dc9d6800b6bfe4d1d42b21
Robin Grindrod <robingrindrod@live.ie>
2018-10-06 00:00:32 +0100

    Add speed

--- Scenes/OneChicken.gd
+++ Scenes/OneChicken.gd
@@ -3,6 +3,7 @@ extends Sprite
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
+export var speed = 1
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
@@ -11,12 +12,12 @@ func _ready():
 
 func _process(delta):
 	if Input.is_action_pressed("ui_left"):
-		position.x -= 1
+		position.x -= speed
 	if Input.is_action_pressed("ui_right"):
-		position.x += 1
+		position.x += speed
 	if Input.is_action_pressed("ui_up"):
-		position.y -= 1
+		position.y -= speed
 	if Input.is_action_pressed("ui_down"):
-		position.y += 1
+		position.y += speed
 	pass
 	
